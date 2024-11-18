fb9f4a3a470d08b8174ad8c60d73c8eb73d28822
Muhamat faissal maulana <114222238+faizisyellow@users.noreply.github.com>
2024-11-18 18:58:45 +0700

    Learn operators : manipulate decimal value to a fixed number

---
+++ javascript-introduction/operators/script.js
@@ -0,0 +1,10 @@
+const myInt = 76;
+const myFloat = 8.1238;
+
+console.log(typeof myInt);
+console.log(typeof myFloat);
+
+// to convert descimal number to a fixed number
+const fixedInt = myFloat.toFixed(0);
+
+console.log(fixedInt);
